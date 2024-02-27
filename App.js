4f428d3de9e1ff42fe4276ff0224be357d66f119
Parth Kapoor <pkapoor_be22@thapar.edu>
2024-02-27 22:47:07 +0530

    Image Pick & Edit Working

--- App.js
+++ App.js
@@ -1,10 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View , Pressable } from 'react-native';
+// import Button from './components/Button';
+import * as ImagePicker from "expo-image-picker"
+import { useState } from 'react';
+
+const PlaceholderImage = require('./assets/placeholder.png');
+
 
 export default function App() {
+
+  const [selectedImage, setSelectedImage] = useState(PlaceholderImage);
+
+  async function handleChangeImage() {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing : true
+    });
+
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0]);
+    }
+    else {
+    }
+  }
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text} >Parth Kapoor</Text>
+      <Image source={selectedImage} style={styles.image} />
+      <Pressable onPress={handleChangeImage} style={styles.pressableButton} >
+        <Text style={styles.text}>
+          Change Image
+        </Text>
+      </Pressable>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +40,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'grey',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    color: 'black',
+    fontSize: 20,
+    marginBottom : 30
+  },
+  image: {
+    height: 320,
+    width: 300,
+    borderRadius : 20
+  },
+    pressableButton: {
+    backgroundColor: 'white',
+    padding: 20,
+    margin: 10,
+    borderRadius : 20
   },
 });
---
+++ components/Button.jsx
@@ -0,0 +1,29 @@
+// import { useState } from "react";
+// import { Pressable, StyleSheet, Text, View } from "react-native";
+
+// export default function Button({ onClick }) {
+  
+//   return (
+//     <View>
+//       <Pressable onPress={onClick} style={styles.pressableButton} >
+//         <Text style={styles.text}>
+//           Change Image
+//         </Text>
+//       </Pressable>
+//     </View>
+//   )
+// }
+
+// const styles = StyleSheet.create({
+  
+//   pressableButton: {
+//     backgroundColor: 'white',
+//     padding: 20,
+//     margin: 10,
+//     borderRadius : 20
+//   },
+//   text: {
+//     fontSize: 20,
+//     alignSelf : 'center'
+//   }
+// })
